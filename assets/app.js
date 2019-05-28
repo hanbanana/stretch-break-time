76ee602b3191ff7aec3681d8c649088d0f490a54
Hansung Lee <hanlee@GOLD-POINT.COM>
2019-05-28 16:19:46 -0700

    add file1

--- assets/app.js
+++ assets/app.js
@@ -1,19 +1,83 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-function move() {
-    var elem = document.getElementById("myBar");   
-    var width = 1;
+ 
+  function move() {
+    var elem = document.getElementById("myProgress");  
+    var countdown =  document.getElementById("counting");
+    var width = 100;
     var id = setInterval(frame, 100);
     function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-      } else {
-        width++; 
+      if (width <= 100) {
+            width--; 
         elem.style.width = width + '%'; 
+        // elem.innerHTML = 100 - width  + '%';        
+      } 
+      if (width <= 0) {
+    clearInterval(id);
       }
     }
+
+
+    var seconds = document.getElementById("countdown").textContent;
+    var countdown = setInterval(function() {
+        seconds--;
+        (seconds == 1) ? document.getElementById("plural").textContent = "": document.getElementById("plural").textContent = "s";
+        document.getElementById("countdown").textContent = seconds;
+        if (seconds <= 0) clearInterval(countdown);
+    }, 1000);
+
   }
-  move();
+
+  $("#start").on("click", function() {
+    move();
+  })
+
+  
+  // function move() {
+  //   var elem = document.getElementById("myProgress");   
+  //   var width = 100;
+  //   var id = setInterval(frame, 100);
+  //   function frame() {
+  //     if (width <= 100) {
+  //           width--; 
+  //       elem.style.width = width + '%'; 
+  //       elem.innerHTML = 100 - width  + '%';        
+  //     } 
+  //     if (width <= 0) {
+  //   clearInterval(id);
+  //     }
+  //   }
+  // }
+
+  // $("#start").on("click", function() {
+  //   move();
+  // })
+
+
+
+
+
+  // $("#start").on("click", function () {
+
+  //   $(".progress-bar").animate({
+  //     width: "100%"
+  //   }, 10000);
+
+  //   var timeleft = 10;
+  //   var downloadTimer = setInterval(function () {
+  //     document.getElementById("counting").innerHTML = timeleft + " seconds remaining";
+  //     timeleft -= 1;
+  //     if (timeleft <= 0) {
+  //       clearInterval(downloadTimer);
+  //       document.getElementById("counting").innerHTML = "Finished"
+  //     }
+  //   }, 1000);
+
+  // })
+
+
+
+
 
 })
