851009c16e148763e31a05af78f5473117786810
Hansung Lee <hanlee@GOLD-POINT.COM>
2019-05-28 16:40:27 -0700

    add file2

--- assets/app.js
+++ assets/app.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     var seconds = document.getElementById("countdown").textContent;
     var countdown = setInterval(function() {
         seconds--;
-        (seconds == 1) ? document.getElementById("plural").textContent = "": document.getElementById("plural").textContent = "s";
+        // (seconds == 1) ? document.getElementById("plural").textContent = "": document.getElementById("plural").textContent = "s";
         document.getElementById("countdown").textContent = seconds;
         if (seconds <= 0) clearInterval(countdown);
     }, 1000);
